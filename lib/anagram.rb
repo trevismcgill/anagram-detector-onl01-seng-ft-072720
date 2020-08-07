cce4a969c1fa613805b8dd9ce9106e7f73b1ea5f
Trevis McGill <trevismcgill25@gmail.com>
2020-08-07 22:34:16 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+require "pry"
+class Anagram
+attr_accessor :word
+
+@@words = []
+def initialize(word)
+# binding.pry
+@word = word
+@@words << @word
+end
+
+def match(array)
+  array.select do |word|
+    binding.pry
+    (@word.split("").sort) == (word.split("").sort)
+  end
+end
+
+
+end
